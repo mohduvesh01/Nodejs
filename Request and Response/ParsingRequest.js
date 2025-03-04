e4de2fa7dfd1a21c18949c163d7de0523c0f618d
Mohd_Uvesh <mohduvesh01@gmail.com>
2025-03-04 15:47:00 +0530

    message

---
+++ Request and Response/ParsingRequest.js
@@ -0,0 +1,60 @@
+const http = require('http');
+const fs = require('fs');
+const { buffer } = require('stream/consumers');
+const server = http.createServer((req,res)=>{
+    console.log(req.url, req.method);
+    if(req.url === '/'){
+        res.setHeader('Content-type','text/html');
+        res.write(`
+            <html>
+            <head><title>ReadingChuncks</title></head>
+            <body>
+        <form action="/submit-details" method="POST">
+            <h1>Enter your details</h1>
+            <input type="text" name="username" placeholder="Enter your name">
+            <br><label for="male">Male</label>
+            <input type="radio" id="male" name="gender" value="male">
+            <label for="female">Female</label>
+            <input type="radio" id="female" name="gender" value="female">
+            <br><input type="submit" value="Submit">
+        </form>
+            </html>
+            `)
+        return res.end();
+    }
+    else if( req.url.toLowerCase() === '/submit-details' && req.method == 'POST'){
+        // Reading Chunk
+      const body = [];
+        req.on('data', chunk =>{
+            console.log(chunk);
+            body.push(chunk);
+        req.on('end',()=>{
+            const fullBody = Buffer.concat(body).toString();
+            console.log(fullBody);
+            //Parsing Request
+            const params = new URLSearchParams(fullBody);
+            const getObj= Object.fromEntries(params);
+            console.log(getObj);
+            fs.writeFileSync('getUser.txt',JSON.stringify(getObj));
+        })
+        })
+
+       
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+    }
+    res.setHeader('Content-type','text/html');
+    res.write(`
+        <html>
+        <head><title>I'm on submit page'</title></head>
+        <body><p>Like, share, Subscribe</p></body>
+        </html>
+        `)
+
+   
+})
+
+const port = 3000;
+server.listen(port,()=>{
+    console.log(`Server is running on address http://localhost:${port}`);
+})
