12ca3837e8920560ab28c97f96faa551aa7a87eb
Mohd_Uvesh <mohduvesh01@gmail.com>
2025-03-07 16:51:23 +0530

    message

---
+++ NPM/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+  console.log(req);  
+})
+
+const port =3000;
+
+server.listen(port,()=>{
+    console.log(`Server is running on address http://localhost:${port}`);
+})
